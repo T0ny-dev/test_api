66ce476c6372a9c28f8894bf98a99c42ca55becc
Jose <jose.zunigahenriquez@gmail.com>
2022-08-19 16:57:56 +0200

    ✅ configure test environment

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Login} from '../src/components/login';
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
+import Login from '../src/components/login';
 
 
 test ('render button submit', async () => {
+  render(<Login />);
   const getEmail = screen.getByRole('inputEmail');
   const getDiscord = screen.getByRole('inputDiscord');
   const getsubmit = screen.getByRole('submit');
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,6 @@ import './Login.css'
 import { useState } from 'react';
 import axios from 'axios';
 
-
-
 function Login () {
   
   const [email1, setEmail] = useState("") 
@@ -12,31 +10,30 @@ function Login () {
   const [logged, setLogged] = useState(false);
 
   const handleSubmit = (e) => {
-  e.preventDefault();
-  alert("el formulario ha sido enviado")
-    axios.post(`https://ms-discord-upy5mhs63a-rj.a.run.app/auth/login`,{'email': `${email1}`, 'discordId':`${discordId1}`} )
-    .then((response) => {
-      console.log(response);
-      setLogged(true);
-    });
+    e.preventDefault();
+    alert("el formulario ha sido enviado")
+      axios.post(`https://ms-discord-upy5mhs63a-rj.a.run.app/auth/login`,{'email': `${email1}`, 'discordId':`${discordId1}`} )
+      .then((response) => {
+        console.log(response);
+        setLogged(true);
+      });
     console.log(email1);
     console.log(discordId1);
   }
 
   return (
     <div>
-    {
-      logged ? <div data-testid="welcome">Hello are you logged</div> : 
-      <form className='Container' onSubmit={handleSubmit}>
-      <label htmlFor="e-maill"><b>Email</b></label>
-      <input  role="inputEmail" type="text" placeholder="Enter e-mail" name="email" required value={email1} onChange={(e)=> setEmail(e.target.value)}></input>
-      <label htmlFor="id-discord"><b>Id Discord</b></label>
-      <input role="inputDiscord" type="text" placeholder="Enter id discord" name="id-discord" required value={discordId1} onChange={(e)=> setdiscordId(e.target.value)}></input>
-      <button role="submit" type="submit">Login</button>
-    </form>
-    }</div>
-
-    
+      {
+        logged ? <div data-testid="welcome">Hello are you logged</div> : 
+        <form className='Container' onSubmit={handleSubmit}>
+          <label htmlFor="e-maill"><b>Email</b></label>
+          <input  role="inputEmail" type="text" placeholder="Enter e-mail" name="email" required value={email1} onChange={(e)=> setEmail(e.target.value)}></input>
+          <label htmlFor="id-discord"><b>Id Discord</b></label>
+          <input role="inputDiscord" type="text" placeholder="Enter id discord" name="id-discord" required value={discordId1} onChange={(e)=> setdiscordId(e.target.value)}></input>
+          <button role="submit" type="submit">Login</button>
+        </form>
+      }
+    </div>
   );
 }
 
